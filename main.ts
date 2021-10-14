429a81d63134a6af8c77386b514f091eb0a8e014
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2021-10-14 15:51:34 +0800

    開始製作，依序繞圈亮燈

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+let 燈號 = 0
+let 暫存 = 10
+for (let index = 0; index < 25; index++) {
+    燈號 += 1
+    if (燈號 < 6) {
+        led.plotBrightness(燈號 - 1, 0, 255)
+    } else if (燈號 < 9) {
+        led.plotBrightness(4, 燈號 - 5, 255)
+    } else if (燈號 < 14) {
+        暫存 += -1
+        led.plotBrightness(燈號 - 暫存, 4, 255)
+    } else if (燈號 < 17) {
+    	
+    } else {
+    	
+    }
+    basic.pause(500)
+}
 basic.forever(function () {
 	
 })
