b129589a5036846dac24853a59e3c9422e79e714
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2021-10-28 16:51:28 +0800

    製作往內繞圈，似乎有點問題

--- main.ts
+++ main.ts
@@ -1,21 +1,45 @@
+function 燈漸滅 () {
+	
+}
+function 燈漸漸滅 (X: number, Y: number) {
+    for (let index = 0; index < 100; index++) {
+        led.plotBrightness(X, Y, led.brightness() - 2.55)
+        basic.pause(10)
+    }
+}
+let Y = 0
+let X = 0
 let 燈號 = 0
-let 暫存 = 10
+let 亮度 = 10
 for (let index = 0; index < 25; index++) {
     燈號 += 1
     if (燈號 < 6) {
-        led.plotBrightness(燈號 - 1, 0, 255)
+        X = 燈號 - 1
+        Y = 0
     } else if (燈號 < 9) {
-        led.plotBrightness(4, 燈號 - 5, 255)
+        X = 4
+        Y = 燈號 - 5
     } else if (燈號 < 14) {
-        暫存 += -1
-        led.plotBrightness(燈號 - 暫存, 4, 255)
-    } else if (燈號 < 17) {
-    	
+        X = 13 - 燈號
+        Y = 4
+    } else if (燈號 < 16) {
+        X = 0
+        Y = 17 - 燈號
+    } else if (燈號 < 20) {
+        X = 燈號 - 16
+        Y = 1
+    } else if (燈號 == 20) {
+        X = 3
+        Y = 2
+    } else if (燈號 < 24) {
+        X = 24 - 燈號
+        Y = 3
     } else {
-    	
+        X = 燈號 - 23
+        Y = 2
     }
-    basic.pause(500)
 }
+led.plotBrightness(X, Y, 255)
 basic.forever(function () {
 	
 })
