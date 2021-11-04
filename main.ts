dbcda59ce5e6fc3c8af3249b0f9117ff7befd9ca
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2021-11-04 16:15:22 +0800

    做完斜線

--- main.ts
+++ main.ts
@@ -2,7 +2,50 @@ let 燈號 = 0
 let X = 0
 let Y = 0
 let 行數 = 0
-function 切換1 () {
+input.onButtonPressed(Button.A, function () {
+    燈號 = 0
+    for (let index = 0; index < 25; index++) {
+        燈號 += 1
+        basic.pause(50)
+        亮起()
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+            break;
+        }
+    }
+    basic.pause(50)
+    校正()
+})
+function 切換 () {
+    led.setBrightness(255)
+    led.toggle(X, Y)
+}
+input.onButtonPressed(Button.AB, function () {
+    for (let 行數 = 0; 行數 <= 4; 行數++) {
+        X = 行數
+        Y = 0
+        for (let index = 0; index < 行數 + 1; index++) {
+            切換()
+            X += -1
+            Y += 1
+            basic.pause(50)
+        }
+        basic.pause(50)
+    }
+    行數 = 4
+    for (let index = 0; index < 4; index++) {
+        行數 += 1
+        X = 4
+        Y = 行數 - 4
+        for (let index = 0; index < 9 - 行數; index++) {
+            切換()
+            X += -1
+            Y += 1
+            basic.pause(50)
+        }
+        basic.pause(50)
+    }
+})
+function 亮起 () {
     if (燈號 < 6) {
         X = 燈號 - 1
         Y = 0
@@ -28,33 +71,12 @@ function 切換1 () {
         X = 燈號 - 23
         Y = 2
     }
-    led.setBrightness(255)
-    led.toggle(X, Y)
+    切換()
 }
-input.onButtonPressed(Button.A, function () {
-    燈號 = 0
-    for (let index = 0; index < 25; index++) {
-        燈號 += 1
-        basic.pause(50)
-        切換1()
-        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
-            break;
-        }
-    }
-    basic.pause(50)
-    校正()
-})
-input.onButtonPressed(Button.AB, function () {
-    燈號 = 0
-    行數 = 0
-    for (let index = 0; index < 25; index++) {
-    	
-    }
-})
 input.onButtonPressed(Button.B, function () {
     燈號 = 25
     for (let index = 0; index < 25; index++) {
-        切換1()
+        亮起()
         燈號 += -1
         basic.pause(50)
         if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
